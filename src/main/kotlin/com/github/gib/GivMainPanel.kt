7149f5dcd9b2bb38f662dff8e7976fbf62152f9a
Piotr Laskowski <piotrek@echo.waw.pl>
2021-02-08 13:47:57 +0100

    Add reload, zoom in and zoom out actions to toolbar

--- src/main/kotlin/com/github/gib/GivMainPanel.kt
+++ src/main/kotlin/com/github/gib/GivMainPanel.kt
@@ -5,11 +5,19 @@ import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.SideBorder
 import com.intellij.ui.jcef.JBCefBrowser
-import com.intellij.ui.layout.panel
 import com.intellij.util.ui.UIUtil
 import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.ImageIcon
+import javax.swing.JButton
 import javax.swing.JPanel
 import javax.swing.JTextField
+import javax.swing.JToolBar
+
+private const val ZOOM_IN_STEP = 1.25
+private const val ZOOM_OUT_STEP = 0.75
+private const val TOOLBAR_ICON_WIDTH = 32
+private const val TOOLBAR_ICON_HEIGHT = 30
 
 @Suppress("UnstableApiUsage")
 class GivMainPanel : SimpleToolWindowPanel(true, true), Disposable {
@@ -30,17 +38,41 @@ class GivMainPanel : SimpleToolWindowPanel(true, true), Disposable {
 //        myDevToolsBrowser.age
 
         val divPanel = JPanel(BorderLayout())
-        divPanel.border = IdeBorderFactory.createBorder(UIUtil.getBoundsColor(), SideBorder.ALL)
+        val border = IdeBorderFactory.createBorder(UIUtil.getBoundsColor(), SideBorder.ALL)
+        divPanel.border = border
         divPanel.add(jbCefBrowser.component, BorderLayout.CENTER)
         setContent(divPanel)
 
-        val myUrlBar = JTextField(url)
-        val panel = panel {
-            row { myUrlBar() }
-        }
+        val toolbar = buildToolbar(jbCefBrowser)
+        toolbar.border = border
+        divPanel.add(toolbar, BorderLayout.NORTH)
+    }
 
-        myUrlBar.addActionListener { jbCefBrowser.loadURL(myUrlBar.text) }
-        divPanel.add(panel, BorderLayout.NORTH)
+    private fun buildToolbar(jbCefBrowser: JBCefBrowser): JToolBar {
+        val toolbar = JToolBar()
+        val urlTextField = JTextField(url)
+        val refreshButton = createToolbarIconButton("/actions/refresh.png")
+        val zoomInButton = createToolbarIconButton("/general/zoomIn.png")
+        val zoomOutButton = createToolbarIconButton("/general/zoomOut.png")
+
+        urlTextField.addActionListener { jbCefBrowser.loadURL(urlTextField.text) }
+        refreshButton.addActionListener { jbCefBrowser.cefBrowser.reload() }
+        zoomInButton.addActionListener { jbCefBrowser.zoomLevel *= ZOOM_IN_STEP }
+        zoomOutButton.addActionListener { jbCefBrowser.zoomLevel *= ZOOM_OUT_STEP }
+
+        toolbar.add(urlTextField)
+        toolbar.add(refreshButton)
+        toolbar.add(zoomInButton)
+        toolbar.add(zoomOutButton)
+        return toolbar
+    }
+
+    private fun createToolbarIconButton(iconPath: String): JButton {
+        val button = JButton()
+        button.icon = ImageIcon(javaClass.getResource(iconPath))
+        button.preferredSize = Dimension(TOOLBAR_ICON_WIDTH, TOOLBAR_ICON_HEIGHT)
+        button.text = ""
+        return button
     }
 
     override fun dispose() {}
