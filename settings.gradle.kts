91033c2a839e66630ed317759670d68a5b7bde7a
Jonathan Gafner <jgafner@gmail.com>
2023-12-02 20:38:59 +0200

    Fix verification compatability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,15 @@
 rootProject.name = "GBrowser"
 
 
-pluginManagement {
-  repositories {
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    gradlePluginPortal()
-  }
-}
-
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+  id("com.gradle.enterprise") version ("3.14.1")
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    publishOnFailure()
+  }
 }
