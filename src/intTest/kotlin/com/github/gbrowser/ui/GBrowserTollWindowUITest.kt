97ea84c2673bbb484f15aa2ad02befc503fd2685
Jonathan Gafner <jgafner@gmail.com>
2023-12-03 13:49:21 +0200

    Align with gradle 8.5

--- src/intTest/kotlin/com/github/gbrowser/ui/GBrowserTollWindowUITest.kt
+++ src/intTest/kotlin/com/github/gbrowser/ui/GBrowserTollWindowUITest.kt
@@ -6,7 +6,6 @@ import com.github.gbrowser.extensions.RemoteRobotExtension
 import com.github.gbrowser.extensions.StepsLogger
 import com.github.gbrowser.fixture.*
 import com.intellij.remoterobot.RemoteRobot
-import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.stepsProcessing.step
