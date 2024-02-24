608e214559a0363c3d6e1192ebc8e4757003863d
JG9 <jgafner@gmail.com>
2024-02-24 13:15:20 +0200

    Test fork in gh 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.intellij.tasks.RunPluginVerifierTask
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
 
+////
 plugins {
 
   id("java") // Java support
