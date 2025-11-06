74bf32a12a2ccedeaaa62d42003b88d63ee62c8a
Jonathan Gafner <jgafner@dorkag.com>
2025-11-06 09:35:36 +0200

    Update Gradle and plugin versions; fix threading issues and improve changelog

--- src/uiTest/kotlin/com/github/gbrowser/ui/Setup.kt
+++ src/uiTest/kotlin/com/github/gbrowser/ui/Setup.kt
@@ -33,7 +33,7 @@ class Setup {
     fun setupTestContext(hyphenateWithClass: String): IDETestContext {
 
       val testCase = TestCase(
-        IdeProductProvider.IU.copy(buildNumber = "253.17525.95", buildType = BuildType.EAP.type), NoProject
+        IdeProductProvider.IU.copy(buildNumber = "253.28086.51", buildType = BuildType.EAP.type), NoProject
       )
       return Starter.newContext(testName = hyphenateWithClass, testCase = testCase).apply {
         val pluginPath = System.getProperty("path.to.build.plugin")
