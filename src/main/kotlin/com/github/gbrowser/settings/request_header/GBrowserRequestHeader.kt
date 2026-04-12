1f1f8049413db705c8af87bb7a089e89f70bf6bb
Jonathan Gafner <jgafner@dorkag.com>
2026-04-12 13:09:08 +0300

    test 2

--- src/main/kotlin/com/github/gbrowser/settings/request_header/GBrowserRequestHeader.kt
+++ src/main/kotlin/com/github/gbrowser/settings/request_header/GBrowserRequestHeader.kt
@@ -3,9 +3,16 @@ package com.github.gbrowser.settings.request_header
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GBrowserRequestHeader(var value: String = "", var name: String? = null, var overwrite: Boolean = false,var uriRegex: String = "") {
+data class GBrowserRequestHeader(var value: String = "", var name: String? = null, var overwrite: Boolean = false, var uriRegex: String = "") {
   companion object {
-    @Suppress("ConstPropertyName", "unused")
+    @Suppress("unused")
     const val serialVersionUID: Long = 3523235970041806118L
+
+
+    fun hello() {
+      val x = 3;
+      val y = 4
+      println("hello ${x + y}")
+    }
   }
 }
